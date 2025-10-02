a72fe84fd0280fdedf93be4486162491ca6da2fd
Doğukan <dgo@apptec360.com>
2025-10-02 20:31:13 +0300

    fix: remove testing url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Workspace, Board, ListItem, Card, User } from '../types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: '',
 });
 
 api.interceptors.request.use((config) => {
